388d9a74d8de5f7a6006d87a8d025bc84f9557ff
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-08-26 11:45:30 -0300

    26082019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './pages/nav/nav.component';
+import { AddUsuarioComponent } from './pages/add-usuario/add-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AddUsuarioComponent
   ],
   imports: [
     BrowserModule,
