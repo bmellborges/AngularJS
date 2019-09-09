5ce56c5cfc4067f2a3a515be2fedd06792c94c7e
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-09-09 09:41:34 -0300

    09092019

--- src/app/pages/add-endereco/endereco.component.ts
+++ src/app/pages/add-endereco/endereco.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Endereco } from 'src/app/model/endereco';
+import { EnderecoService } from 'src/app/services/endereco.service';
 
 @Component({
   selector: 'app-endereco',
@@ -10,9 +11,14 @@ export class EnderecoComponent implements OnInit {
 
   @Input() public endereco: Endereco = new Endereco;
 
-  constructor() { }
+  constructor(
+    protected enderecoService:EnderecoService
+  ) { }
 
   ngOnInit() {
   }
 
+  buscaCEP(){
+    this.endereco = this.enderecoService.getEndereco(this.endereco.cep);
+  }
 }
--- src/app/services/endereco.service.ts
+++ src/app/services/endereco.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Endereco } from '../model/endereco';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnderecoService {
 
-  constructor() { }
+  protected endereco: Endereco = new Endereco;
+
+  constructor(
+    protected http: HttpClient
+  ) { }
+
+  getEndereco(cep) {
+    this.http.get<Endereco>("https://viacep.com.br/ws/" + cep + "/json/")
+      .subscribe(
+        res => {
+          this.endereco = res;
+          console.log(res);
+        },
+        erro => {
+          this.endereco = null;
+          console.log(erro);
+        }
+      )
+    return this.endereco
+  }
+
 }
